a6201b6d792b2f3e96ae72d08e08e9b62ec4d3d4
Ragul VR <hema@1990>
2020-03-25 08:26:18 +0530

    Pascals triangle 2

---
+++ Easy/119. Pascals Triangle 2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        if(rowIndex == 0)
+            return {1};
+        if(rowIndex == 1)
+            return {1,1};
+        
+        vector<int> res(rowIndex+1,1);
+        
+        for(int i=2;i<=rowIndex;i++)
+        {
+            for(int j=i-1;j>=1;j--)
+                res[j] = res[j] + res[j-1];     
+        }    
+       return res; 
+    }
+};
