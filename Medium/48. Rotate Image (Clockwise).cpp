058f1b63156154600f3766d162abc49fab0a9686
Ragul VR <hema@1990>
2020-03-31 09:27:04 +0530

    Matrix both clockwise and anticlockwise rotate

--- Medium/48. Rotate Image (Clockwise).cpp
+++ Medium/48. Rotate Image (Clockwise).cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
         
-        int n = matrix.size();
-        reverse(matrix.begin(),matrix.end());
-        
-        for(int i=0;i<n;i++)
-            for(int j=0;j<i;j++)
-                swap(matrix[i][j],matrix[j][i]);
-    }
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for(int i=0;i<n;i++)
+        for(int j=0;j<i;j++)
+           swap(matrix[i][j],matrix[j][i]);
+}
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+void anti_rotate(vector<vector<int> > &matrix) {
+    for (auto vi : matrix) reverse(vi.begin(), vi.end());
+    for(int i=0;i<n;i++)
+        for(int j=0;j<i;j++)
+            swap(matrix[i][j],matrix[j][i]);
+}
+    
 };
