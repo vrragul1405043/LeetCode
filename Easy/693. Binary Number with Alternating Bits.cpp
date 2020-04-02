9d778b0fa36da6552750da71338df74f25b24003
Ragul VR <hema@1990>
2020-04-02 19:59:44 +0530

    Binary Number with Alternating bits

---
+++ Easy/693. Binary Number with Alternating Bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int last = n%2;
+        n>>=1;
+        
+        while(n)
+        {
+            int current = n%2;
+            if(current == last)
+                return false;
+            last = current;
+            n>>=1;
+        }
+        return true;
+    }
+};
