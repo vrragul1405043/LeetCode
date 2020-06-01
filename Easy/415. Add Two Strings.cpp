946d16821fca46bf76a570778b8bba312cb76a6b
Ragul Ravishankar <ragul.ravishankar@gmail.com>
2020-06-01 07:40:02 +0530

    Add two strings

--- Easy/415. Add Two Strings.cpp
+++ Easy/415. Add Two Strings.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
+    string addStrings(string num1, string num2)
+    {
+
+        string result;
+
+        int num1_iterator = num1.size() - 1;
+        int num2_iterator = num2.size() - 1;
         int carry = 0;
-        string res = "";
-        int i = num1.size()-1;
-        int j = num2.size()-1;
-        while(i>=0 || j>=0 || carry)
-        {
+
+        while (num1_iterator >= 0 || num2_iterator >= 0 || carry > 0) {
             unsigned int sum = 0;
-            if(i>=0)
-                sum+=num1[i--]-'0';
-            if(j>=0)
-                sum+=num2[j--]-'0';
-            
-            sum+=carry;
-            carry = sum/10;
-            sum = sum%10;
-            res+=to_string(sum);
-        } 
-        reverse(res.begin(),res.end());
-        return res;
+            sum += (num1_iterator >= 0) ? num1[num1_iterator--] - '0' : 0;
+            sum += (num2_iterator >= 0) ? num2[num2_iterator--] - '0' : 0;
+            sum += carry;
+            result = to_string(sum % 10) + result;
+            carry = sum / 10;
+        }
+        return result;
     }
 };
